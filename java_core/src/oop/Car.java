79442564c393dc9c32124105feadef4bee1062ea
== <==>
2019-12-02 19:59:16 +0700

    create package oop

---
+++ java_core/src/oop/Car.java
@@ -0,0 +1,32 @@
+package oop;
+
+public class Car {
+//	Thuộc tính  === những biến
+		String color;
+		int height;
+		int weight;
+		int speed;
+		String s = "Bạn đã lấy được thuộc tính của class.";
+		// khởi tạo hành vi Stop()
+		public void Stop() {
+
+
+		}
+		// khởi tạo hành vi turnLeft
+		public void turnLeft() {
+			// TODO Auto-generated method stub
+
+		}
+		
+		public static void main(String[] args)
+		{
+			
+			Car audi = new Car();
+			System.out.println(audi.s);
+			
+		}
+		
+		
+		
+		
+}
